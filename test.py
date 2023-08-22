040b57cff7138d39809aba638cf949e433f49972
Colton Rhodes <colton.rhodes@oit.edu>
2023-08-21 22:59:30 -0700

    added sqlite to the code

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
 import tkinter as tk
+import sqlite3
+
+conn = sqlite3.connect('Bartending-App\database.db')
+
 window = tk.Tk()
 window.title("Bartending App")
 window.geometry("400x400")
 label = tk.Label(text="Hello, Bartender", font=("Times New Roman", 20, "bold"))
-label.pack(pady=20)
+label.pack(pady=5)
+search_box = tk.Entry(window)
+search_box.pack(pady=20)
+search_box.pack()
+def on_search_pressed():
+    print(search_box.get())
+search_box.bind("<Return>", on_search_pressed())
 window.mainloop()
